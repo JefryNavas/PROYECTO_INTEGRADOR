57cfc4d66e107a8facb77a6be2331aac31ad4633
Jefry Navas <jnavass@est.ups.edu.ec>
2020-07-17 14:35:20 -0500

    REGISTRAR USUARIO

---
+++ src/main/java/com/camas/model/Usuario.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.camas.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Jeffry
+ */
+@Entity
+@Table(name="usuario")
+public class Usuario implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_usuario;
+    @Column(name = "id_perfil")
+    private int perfil=1;
+    @Column(name = "nombres")
+    private String nombres;
+    @Column(name = "apellidos")
+    private String apellidos;
+    @Column(name = "usuario")
+    private String usuario;
+    @Column(name = "contraseña")
+    private String contraseña;
+
+    public int getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(int perfil) {
+        this.perfil = perfil;
+    }
+
+   
+    public String getUsuario() {
+        return usuario;
+    }
+
+    
+ 
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    
+    
+    
+    public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getNombre_usuario() {
+        return usuario;
+    }
+
+    public void setNombre_usuario(String nombre_usuario) {
+        this.usuario = nombre_usuario;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+
+}
